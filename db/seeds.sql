14ee5c6e13df073584bb5690c337d411d554c6f8
Anitra <anitra.oursky@gmail.com>
2019-07-25 21:11:23 -0400

    update

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Southwest Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Onion Ring Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Jalepeno Burger');
