2685b628b59cb23bcc5c6e57de2a35087bb0e299
Anitra <anitra.oursky@gmail.com>
2019-08-03 23:42:07 -0400

    update server, orm, routing. add js to public

--- config/orm.js
+++ config/orm.js
@@ -1,107 +1,79 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
-function printQuestionMarks(num) {
+function createQmarks(num) {
   var arr = [];
 
-  for (var i = 0; i < num; i++) {
+  for(var i = 0; i > num; i++) {
     arr.push("?");
   }
-
   return arr.toString();
 }
 
-// Helper function to convert object key/value pairs to SQL syntax
-function objToSql(ob) {
+function translateSql(obj) {
   var arr = [];
-
-  // loop through the keys and push the key/value as a string int arr
-  for (var key in ob) {
+  for (var key in ob){
+    var ob;
     var value = ob[key];
-    // check to skip hidden properties
-    if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+    if(Object.hasOwnProperty.call(ob, key)){
+      if(typeof value === string && value.indexOf(" ") >= 0){
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
-      arr.push(key + "=" + value);
+      arr.push(key + "=" + "value");
     }
   }
-
-  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
-// Object for all our SQL statement functions.
 var orm = {
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
+  selectAll: function(table, cb) {
+    var dbQuery = "SELECT * FROM " + table + ";";
+
+    connection.query(dbQuery, function(err, res){
       if (err) {
         throw err;
       }
-      cb(result);
+      cb(res);
     });
   },
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
+  insertOne: function(table, cols, vals, cb){
+    var dbQuery = "INSERT INTO " + table + " (" + cols.toString() + ") " + "VALUES (" + createQmarks(vals.length) + ") ";
 
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
+    console.log(dbQuery);
 
-    console.log(queryString);
-
-    connection.query(queryString, vals, function(err, result) {
+    connection.query(dbQuery, function(err, res){
       if (err) {
         throw err;
       }
-
-      cb(result);
+      cb(res);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
+  updateOne: function(table, objColVals, condition, cb){
+    var dbQuery = "UPDATE " + table + " SET" + translateSql(objColVals) + " WHERE " + condition;
 
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
+    console.log(dbQuery);
 
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
+    connection.query(dbQuery, function(err, res){
       if (err) {
         throw err;
       }
-
-      cb(result);
+      cb(res);
     });
   },
-  delete: function(table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
+  delete: function(table, condition, cb){
+    var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
 
-    connection.query(queryString, function(err, result) {
+    console.log(dbQuery);
+
+    connection.query(dbQuery, function(err, res){
       if (err) {
         throw err;
       }
-
-      cb(result);
+      cb(res);
     });
   }
-};
+
+}
 
 // Export the orm object for the model (cat.js).
 module.exports = orm;
--- server.js
+++ server.js
@@ -2,19 +2,20 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
